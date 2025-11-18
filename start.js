6b082c4fed42f329f446ab13d55bcd418c7c5eef
walter.hafner <walter.hafner@viscomag.ch>
2025-11-18 16:37:51 +0100

    v0.1.4: Remove deprecation warning by using direct binary path instead of shell

--- start.js
+++ start.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
 const { spawn } = require("child_process");
+const path = require("path");
+const os = require("os");
 
 // Get arguments after 'node' and 'start.js'
 const args = process.argv.slice(2);
 
-// Spawn the Rust binary with arguments
-const child = spawn("ngx-translate-lint-rs", args, {
+// Construct the path to the binary
+const binaryName = os.platform() === "win32" ? "ngx-translate-lint-rs.exe" : "ngx-translate-lint-rs";
+const binaryPath = path.join(os.homedir(), ".cargo", "bin", binaryName);
+
+// Spawn the Rust binary with arguments (without shell)
+const child = spawn(binaryPath, args, {
   stdio: "inherit",
-  shell: true
+  shell: false
 });
 
 child.on("exit", (code) => {
